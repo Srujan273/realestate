d35030e5adaa97b35dd92b63e6ce1ef4e70c87b2
Srujan Geedipalli <balasaisrujan273@gmail.com>
2024-10-18 00:12:19 +0530

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+// Initialize express app
 const app = express();
+
+// Start server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-    }
-)
+  console.log('Server is running on port 3000!');
+});
